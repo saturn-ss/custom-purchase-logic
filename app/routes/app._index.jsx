6641af101a9431fab018c46d7dde46a7f7a77a90
saturn-ss <shopify.saturn@outlook.com>
2025-06-10 19:29:20 -0400

    enhance Config component layout and add product selection functionality

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -11,8 +11,13 @@ import {
   List,
   Link,
   InlineStack,
-  TextField
+  TextField,
+  useBreakpoints,
+  InlineGrid,
+  Divider
 } from "@shopify/polaris";
+import { useAppBridge } from "@shopify/app-bridge-react";
+import { ResourcePicker } from '@shopify/app-bridge/actions';
 
 import { authenticate } from "../shopify.server";
 
@@ -44,17 +49,87 @@ return existing;
 export default function Config() {
   const existing = useLoaderData();
   const existingSData = {};
+
+  const app = useAppBridge();
+
+  const handleOpen = () => {
+    const picker = ResourcePicker.create(app, {
+      resourceType: "Product",
+      selectMultiple: false,
+      showVariants: false,
+    })
+
+    picker.subscribe(ResourcePicker.Action.SELECT, (action) => {
+      const [ product ] = action.payload.selection;
+      onselect(product);
+      picker.unsubscride();
+      picker.close();
+    });
+
+    picker.dispatch(ResourcePicker.Action.OPEN);
+  }
+
+  const { smUp } = useBreakpoints();
   return (
-    <Page>
-      <Layout>
-        <BlockStack>
-          <InlineStack>
-            <Text>
-            </Text>
-            <TextField />
-          </InlineStack>
-        </BlockStack>
-      </Layout>
+    <Page
+      divider
+      primaryAction={{ content: "View on your store", disabled: true }}
+      secondaryActions={[
+        {
+          content: "Duplicate",
+          accessibilityLabel: "Secondary action label",
+          onAction: () => alert("Duplicate action"),
+        },
+      ]}
+    >
+      <BlockStack gap={{ xs: "800", sm: "400" }}>
+        <InlineGrid columns={{ xs: "1fr", md: "2fr 5fr" }} gap="400">
+          <Box
+            as="section"
+            paddingInlineStart={{ xs: 400, sm: 0 }}
+            paddingInlineEnd={{ xs: 400, sm: 0 }}
+          >
+            <BlockStack gap="400">
+              <Text as="h3" variant="headingMd">
+                InterJambs
+              </Text>
+              <Text as="p" variant="bodyMd">
+                Interjambs are the rounded protruding bits of your puzzlie piece
+              </Text>
+            </BlockStack>
+          </Box>
+          <Card roundedAbove="sm">
+            <BlockStack gap="400">
+              <TextField label="Interjamb style" />
+              <Button onClick={handleOpen}>Select Product</Button>
+              <TextField label="Interjamb ratio" />
+            </BlockStack>
+          </Card>
+        </InlineGrid>
+        {smUp ? <Divider /> : null}
+        <InlineGrid columns={{ xs: "1fr", md: "2fr 5fr" }} gap="400">
+          <Box
+            as="section"
+            paddingInlineStart={{ xs: 400, sm: 0 }}
+            paddingInlineEnd={{ xs: 400, sm: 0 }}
+          >
+            <BlockStack gap="400">
+              <Text as="h3" variant="headingMd">
+                Dimensions
+              </Text>
+              <Text as="p" variant="bodyMd">
+                Interjambs are the rounded protruding bits of your puzzlie piece
+              </Text>
+            </BlockStack>
+          </Box>
+          <Card roundedAbove="sm">
+            <BlockStack gap="400">
+              <TextField label="Horizontal" />
+              <TextField label="Interjamb ratio" />
+            </BlockStack>
+          </Card>
+        </InlineGrid>
+      </BlockStack>
     </Page>
   );
 }
